7e2f805b971ff37927967f99d5cd459aec5038d4
Shriya <d.shriya@iitg.ac.in>
2021-05-01 03:16:34 +0530

    errors correted

--- app.js
+++ app.js
@@ -109,6 +109,7 @@ app.use((err,req,res,next) =>{
     res.status(statusCode).render('error', {err});
 })
 
+const port = process.env.PORT || 3000;
 app.listen(3000, ()=>{
-    console.log("serving on port 3000");
+    console.log(`serving on port ${port}`);
 })
